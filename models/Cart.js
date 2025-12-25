8db54762675532c2ec3fc69ae913c53222d5fb89
karan-negi01 <btwitskaranhere@gmail.com>
2025-12-25 15:34:44 +0530

    done123456

--- models/Cart.js
+++ models/Cart.js
@@ -91,7 +91,7 @@ cartSchema.methods.calculateAndSave = async function() {
 
 // Get checkout totals (hidden platform fee included in GST)
 cartSchema.methods.getCheckoutTotals = function() {
-  const SHIPPING_CHARGE = 1;
+  const SHIPPING_CHARGE = 299;
   
   // Platform fee calculation (hidden from user)
   let platformFeePercentage = 0;
